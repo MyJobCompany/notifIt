986bef084cb442d289549a0d22a6f5389b275fcc
Julien Mira <julien.mira89@gmail.com>
2014-04-23 19:47:41 +0200

    changes on notification position, now there is a vertical position and a horizontal position. reinitilization of timeout on mouse over, the notification will not close while the mouse is over it. changes on css for being easier to integrate in projects

--- notifIt!-1.1/js/notifIt.js
+++ notifIt!-1.1/js/notifIt.js
@@ -1,13 +1,14 @@
 /*
  * notifIt! by @naoxink
  */
-function notif(config) {
+function notif(datas) {
     var to = null;
     var defaults = {
         type: "info",
         width: 400,
         height: 60,
-        position: "right",
+        horizontal: "center",
+        vertical: "bottom",
         autohide: 1,
         msg: "This is my default message",
         opacity: 1,
@@ -17,144 +18,190 @@ function notif(config) {
         color: "",
         timeout: 5000
     };
-    $.extend(defaults, config);
+    var config = $.extend({} , defaults, datas);
     
-    position = defaults.position;
-
-    if (defaults.width > 0) {
-        defaults.width = defaults.width;
-    } else if (defaults.width === "all") {
-        defaults.width = screen.width - 60;
+    if (config.width === "all") {
+        config.width = screen.width - 60;
     }
 
-    if (defaults.height < 100 && defaults.height > 0) {
-        height = defaults.height;
+    if (config.height < 100 && config.height > 0) {
+        height = config.height;
     }
 
-    var div = "<div id='ui_notifIt'><p>" + defaults.msg + "</p></div>";
-    $("#ui_notifIt").remove();
+    var div = "<div class='ui_notifIt'><p>" + config.msg + "</p></div>";
+    $(".ui_notifIt").remove();
     clearInterval(to);
     $("body").append(div);
 
 
-    if (defaults.multiline) {
-        $("#ui_notifIt").css("padding", 15);
+    if (config.multiline) {
+        $(".ui_notifIt").css("padding", 15);
     } else {
-        $("#ui_notifIt").css("height", height);
-        $("#ui_notifIt p").css("line-height", height + "px");
+        $(".ui_notifIt").css("height", height);
+        $(".ui_notifIt p").css("line-height", height + "px");
     }
 
-    $("#ui_notifIt").css("width", defaults.width);
+    $(".ui_notifIt").css("width", config.width);
 
-    $("#ui_notifIt").css("opacity", defaults.opacity);
+    $(".ui_notifIt").css("opacity", config.opacity);
 
-    switch (defaults.type) {
+    switch (config.type) {
         case "error":
-            $("#ui_notifIt").addClass("error");
+            $(".ui_notifIt").addClass("error");
             break;
         case "success":
-            $("#ui_notifIt").addClass("success");
+            $(".ui_notifIt").addClass("success");
             break;
         case "info":
-            $("#ui_notifIt").addClass("info");
+            $(".ui_notifIt").addClass("info");
             break;
         case "warning":
-            $("#ui_notifIt").addClass("warning");
+            $(".ui_notifIt").addClass("warning");
             break;
         default:
-            $("#ui_notifIt").addClass("default");
+            $(".ui_notifIt").addClass("default");
             break;
     }
 
-    $("#ui_notifIt").css("background-color", defaults.bgcolor);
+    $(".ui_notifIt").css("background-color", config.bgcolor);
     
-    $("#ui_notifIt").css("color", defaults.color);
+    $(".ui_notifIt").css("color", config.color);
     
-    switch (defaults.position) {
-        case "left":
-            $("#ui_notifIt").css("left", parseInt(0 - (defaults.width + 10)));
-            $("#ui_notifIt").css("left", parseInt(0 - (defaults.width * 2)));
-            $("#ui_notifIt").animate({left: 10});
-            break;
-        case "right":
-            $("#ui_notifIt").css("right", parseInt(0 - (defaults.width + 10)));
-            $("#ui_notifIt").css("right", parseInt(0 - (defaults.width * 2)));
-            $("#ui_notifIt").animate({right: 10});
-            break;
-        case "center":
-            var mid = window.innerWidth / 2;
-            $("#ui_notifIt").css("top", parseInt(0 - (defaults.height + 10)));
-            $("#ui_notifIt").css("left", mid - parseInt(defaults.width / 2));
-            $("#ui_notifIt").animate({top: 10});
-            break;
-        default:
-            var mid = window.innerWidth / 2;
-            $("#ui_notifIt").css("right", parseInt(0 - (defaults.width + 10)));
-            $("#ui_notifIt").css("left", mid - parseInt(defaults.width / 2));
-            $("#ui_notifIt").animate({right: 10});
-            break;
+    if(config.horizontal === "left" && config.vertical === "top"){
+        $(".ui_notifIt").css("left", parseInt(0 - (config.width + 10)));
+        $(".ui_notifIt").css("left", parseInt(0 - (config.width * 2)));
+        $(".ui_notifIt").animate({left: 10});
+    }
+    if(config.horizontal === "right" && config.vertical === "top"){
+        $(".ui_notifIt").css("right", parseInt(0 - (config.width + 10)));
+        $(".ui_notifIt").css("right", parseInt(0 - (config.width * 2)));
+        $(".ui_notifIt").animate({right: 10});
+    }
+    if(config.horizontal === "center" && config.vertical === "top"){
+        var mid = window.innerWidth / 2;
+        $(".ui_notifIt").css("top", parseInt(0 - (config.height + 10)));
+        $(".ui_notifIt").css("left", mid - parseInt(config.width / 2));
+        $(".ui_notifIt").animate({top: 10});
+    }
+    if(config.horizontal === "left" && config.vertical === "center"){
+        var mid = window.innerHeight / 2;
+        $(".ui_notifIt").css("top", mid);
+        $(".ui_notifIt").css("left", parseInt(0 - (config.width + 10)));
+        $(".ui_notifIt").css("left", parseInt(0 - (config.width * 2)));
+        $(".ui_notifIt").animate({left: 10});
+    }
+    if(config.horizontal === "right" && config.vertical === "center"){
+        var mid = window.innerHeight / 2;
+        $(".ui_notifIt").css("top", mid);
+        $(".ui_notifIt").css("right", parseInt(0 - (config.width + 10)));
+        $(".ui_notifIt").css("right", parseInt(0 - (config.width * 2)));
+        $(".ui_notifIt").animate({right: 10});
+    }
+    if(config.horizontal === "center" && config.vertical === "center"){
+        var midHeight = window.innerHeight / 2;
+        var midWidth = window.innerWidth / 2;
+        $(".ui_notifIt").css("top", midHeight);
+        $(".ui_notifIt").css("left", midWidth - parseInt(config.width / 2));
+        $(".ui_notifIt").hide();
+        $(".ui_notifIt").fadeIn("slow");
+        config.fade = true;
+    }
+    if(config.horizontal === "left" && config.vertical === "bottom"){
+        $(".ui_notifIt").css('top', 'auto').css("bottom", parseInt(10));
+        $(".ui_notifIt").css("left", parseInt(0 - (config.width + 10)));
+        $(".ui_notifIt").css("left", parseInt(0 - (config.width * 2)));
+        $(".ui_notifIt").animate({left: 10});
+    }
+    if(config.horizontal === "right" && config.vertical === "bottom"){
+        $(".ui_notifIt").css('top', 'auto').css("bottom", parseInt(10));
+        $(".ui_notifIt").css("right", parseInt(0 - (config.width + 10)));
+        $(".ui_notifIt").css("right", parseInt(0 - (config.width * 2)));
+        $(".ui_notifIt").animate({right: 10});
+    }
+    if(config.horizontal === "center" && config.vertical === "bottom"){
+        var mid = window.innerWidth / 2;
+        $(".ui_notifIt").css('top', 'auto').css("bottom", parseInt(0 - (config.height + 10)));
+        $(".ui_notifIt").css("left", mid - parseInt(config.width / 2));
+        $(".ui_notifIt").animate({bottom: 10});
     }
     
-    $("#ui_notifIt").click(function() {
-        notifit_dismiss(to, defaults);
+    $(".ui_notifIt").click(function() {
+        notifit_dismiss(to, config);
     });
 
-    if (defaults.autohide) {
-            if (!isNaN(defaults.timeout)) { // Take the timeout if is a number
-                to = setTimeout(function() {
-                    $("#ui_notifIt").click();
-                }, defaults.timeout);
-            }
-        
+    if (config.autohide) {
+        if (!isNaN(config.timeout)) { // Take the timeout if is a number
+            to = setTimeout(function() {
+                $(".ui_notifIt").click();
+            }, config.timeout);
+        }
     }
+    
+    /*timer resume on hover*/
+    $(".ui_notifIt")
+    .mouseover(function() {
+      clearInterval(to);
+    })
+    .mouseleave(function() {
+      to = setTimeout(function() {
+            $(".ui_notifIt").click();
+        }, config.timeout);
+    });
 }
 
 function notifit_dismiss(to, config) {
     clearInterval(to);
     if (!config.fade) {
-        switch(config.position){
+        switch(config.horizontal){
             case "center":
-                $("#ui_notifIt").animate({
-                    top: parseInt(config.height - (config.height / 2))
-                }, 100, function() {
-                    $("#ui_notifIt").animate({
-                        top: parseInt(0 - (config.height * 2))
+                if(config.vertical === "top"){
+                    $(".ui_notifIt").animate({
+                        top: parseInt(config.height - (config.height / 2))
                     }, 100, function() {
-                        $("#ui_notifIt").remove();
+                        $(".ui_notifIt").animate({
+                            top: parseInt(0 - (config.height * 2))
+                        }, 100, function() {
+                            $(".ui_notifIt").remove();
+                        });
                     });
-                });
+                }else{
+                    $(".ui_notifIt").animate({
+                        bottom: parseInt(config.height - (config.height / 2))
+                    }, 100, function() {
+                        $(".ui_notifIt").animate({
+                            bottom: parseInt(0 - (config.height * 2))
+                        }, 100, function() {
+                            $(".ui_notifIt").remove();
+                        });
+                    });
+                }
             break;
             case "right":
-                $("#ui_notifIt").animate({
+                $(".ui_notifIt").animate({
                     right: parseFloat(config.width - (config.width * 0.9))
                 }, 100, function() {
-                    $("#ui_notifIt").animate({
+                    $(".ui_notifIt").animate({
                         right: parseInt(0 - (config.width * 2))
                     }, 100, function() {
-                        $("#ui_notifIt").remove();
+                        $(".ui_notifIt").remove();
                     });
                 });
             break;
             case "left":
-                $("#ui_notifIt").animate({
+                $(".ui_notifIt").animate({
                     left: parseFloat(config.width - (config.width * 0.9))
                 }, 100, function() {
-                    $("#ui_notifIt").animate({
+                    $(".ui_notifIt").animate({
                         left: parseInt(0 - (config.width * 2))
                     }, 100, function() {
-                        $("#ui_notifIt").remove();
+                        $(".ui_notifIt").remove();
                     });
                 });
             break;
         }
     } else {
-        $("#ui_notifIt").fadeOut("slow", function() {
-            $("#ui_notifIt").remove();
+        $(".ui_notifIt").fadeOut("slow", function() {
+            $(".ui_notifIt").remove();
         });
     }
 }
-notif({
-    position: "center",
-    autohide: 0,
-    width: "all"
-});
